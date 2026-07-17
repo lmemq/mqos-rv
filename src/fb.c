58ce7057d4111c2a0c7571de20b2bf73813ccf73
lmemq <179820070+lmemq@users.noreply.github.com>
2026-07-17 13:36:41 +0300

    Removed deprecated methods

--- src/fb.c
+++ src/fb.c
@@ -110,33 +110,3 @@ void flush() {
     kmemcpy((void*)fb.fb_addr, (void*)back_buffer, fb.fb_size);
     asm volatile("fence w, o" ::: "memory");
 }
-
-void write_xrgb256_pixel(uint16_t x, uint16_t y, uint8_t pixel[4]){ // deprecated
-    kmemcpy((void*)fb.fb_addr + ((y * fb.fb_stride) + (x * fb.fb_bpp)), pixel, 4);
-}
-
-
-void write_rgb256_pixel(uint16_t x, uint16_t y, uint8_t pixel[3]) { // deprecated
-    // offset one byte (xrgb)
-    kmemcpy((void*)fb.fb_addr + ((y * fb.fb_stride) + (x * fb.fb_bpp)), pixel, 4);
-}
-
-void draw_rgb256_map(uint32_t x_res, uint32_t y_res, uint8_t *rgb_map) {
-
-    uint8_t map_bpp = 4;
-    uint64_t map_stride = x_res * map_bpp;
-    uint64_t map_size = map_stride*y_res;
-
-    uint64_t i = 0;
-    for (int map_i = 0; map_i < map_size; map_i += 4) {
-        if (map_i%map_stride == 0 && map_i != 0) {
-            i += fb.fb_stride-map_stride;
-        }
-        // 1 compensates for alignement (xRGB)
-        kmemcpy((void*)fb.fb_addr + i, &rgb_map[map_i], 4);
-
-        i += 4;
-    }
-} // deprecated
-
-
